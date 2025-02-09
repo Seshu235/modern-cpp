88695b92de37b06c86768542d704ec9200f59452
Seshu235 <seshusaibabupothuraju@gmail.com>
2025-02-09 16:12:17 +0530

    updated code
    Signed-off-by: Seshu235 <seshusaibabupothuraju@gmail.com>

--- stl_iterator/vector_iterator2.cpp
+++ stl_iterator/vector_iterator2.cpp
@@ -5,9 +5,9 @@
 int main() {
     std::vector<int> v = {1, 2, 3};
 
-    std::vector<int>::iterator it;
+    // std::vector<int>::iterator it;
     std::cout << "Using iterator for insert...\n";
-    for (it = v.begin(); it != v.end(); it++) {
+    for (auto it = v.begin(); it != v.end(); it++) {
         if (it == v.begin())
             it = v.insert(it, 5);
         std::cout << *it << " ";
@@ -15,23 +15,21 @@ int main() {
     std::cout << std::endl;
 
     std::cout << "Using iterator for delete...\n";
-    for (it = v.begin(); it != v.end(); it++) {
+    for (auto it = v.begin(); it != v.end(); it++) {
         if (it == v.begin())
             it = v.erase(it);
         std::cout << *it << " ";
     }
     std::cout << std::endl;
 
-    std::vector<int>::reverse_iterator rt;    
     std::cout << "Using reverse_iterator...\n";
-    for (rt = v.rbegin(); rt != v.rend(); rt++) {
+    for (auto rt = v.rbegin(); rt != v.rend(); rt++) {
         std::cout << *rt << " ";
     }
     std::cout << std::endl;
 
-    std::vector<int>::const_iterator it1;
     std::cout << "Using const_iterator...\n";
-    for (it1 = v.cbegin(); it1 != v.cend(); it1++) {
+    for (auto it1 = v.cbegin(); it1 != v.cend(); it1++) {
         std::cout << *it1 << " ";
     }
     std::cout << std::endl;
